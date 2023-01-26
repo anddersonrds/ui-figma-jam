578d4cae7030cbb70e0719500cfa7976a8cd8b12
Anderson Rodrigues <anddersonrds@gmail.com>
2023-01-26 19:53:13 -0300

    feat: add background and controls

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,18 @@
+import ReactFlow, { Background, Controls } from 'reactflow'
+import { zinc } from 'tailwindcss/colors'
+import 'reactflow/dist/style.css'
+
+import '@/styles/global.css'
+
 function App() {
-  return <h1>Hello World!</h1>
+  return (
+    <div className="w-screen h-screen">
+      <ReactFlow>
+        <Background gap={12} size={2} color={zinc[200]} />
+        <Controls />
+      </ReactFlow>
+    </div>
+  )
 }
 
 export default App
