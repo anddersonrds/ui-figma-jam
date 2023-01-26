997174841c9bc199da8e601654035663ca87ab54
Anderson Rodrigues <anddersonrds@gmail.com>
2023-01-26 19:32:47 -0300

    chore: add absolute paths

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,14 @@
-import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
 
-// https://vitejs.dev/config/
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const path = require('path')
+
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, 'src')
+    }
+  }
 })
