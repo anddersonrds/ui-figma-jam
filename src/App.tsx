5877734bdba2c179250be8a099d34203d8722620
Anderson Rodrigues <anddersonrds@gmail.com>
2023-01-26 21:14:01 -0300

    feat: add nodes and edges to whitebord

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,81 @@
-import ReactFlow, { Background, Controls } from 'reactflow'
+import { useCallback } from 'react'
+import ReactFlow, {
+  addEdge,
+  Background,
+  Connection,
+  ConnectionMode,
+  Controls,
+  Node,
+  useEdgesState,
+  useNodesState
+} from 'reactflow'
 import { zinc } from 'tailwindcss/colors'
 import 'reactflow/dist/style.css'
 
+import { DefaultEdge } from '@/components/edges/default-edge'
+import { Square } from '@/components/nodes/square'
+
 import '@/styles/global.css'
 
-function App() {
+const NODE_TYPES = {
+  square: Square
+}
+
+const EDGE_TYPES = {
+  default: DefaultEdge
+}
+
+const INITIAL_NODES = [
+  {
+    id: crypto.randomUUID(),
+    type: 'square',
+    position: {
+      x: 200,
+      y: 400
+    },
+    data: {
+      text: ''
+    }
+  },
+  {
+    id: crypto.randomUUID(),
+    type: 'square',
+    position: {
+      x: 1000,
+      y: 400
+    },
+    data: {
+      text: ''
+    }
+  }
+] as Node[]
+
+export const App = () => {
+  const [edges, setEdges, onEdgesChange] = useEdgesState([])
+  const [nodes, setNodes, onNodesChange] = useNodesState(INITIAL_NODES)
+
+  const onConnect = useCallback((connection: Connection) => {
+    return setEdges((edges) => addEdge(connection, edges))
+  }, [])
+
   return (
     <div className="w-screen h-screen">
-      <ReactFlow>
+      <ReactFlow
+        nodeTypes={NODE_TYPES}
+        edgeTypes={EDGE_TYPES}
+        nodes={nodes}
+        edges={edges}
+        onConnect={onConnect}
+        onEdgesChange={onEdgesChange}
+        onNodesChange={onNodesChange}
+        connectionMode={ConnectionMode.Loose}
+        defaultEdgeOptions={{
+          type: 'default'
+        }}
+      >
         <Background gap={12} size={2} color={zinc[200]} />
         <Controls />
       </ReactFlow>
     </div>
   )
 }
-
-export default App
